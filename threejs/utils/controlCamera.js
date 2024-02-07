e4d477ee8821da819b738cc9532cb169d69843fb
glutonium69 <146271709+glutonium69@users.noreply.github.com>
2024-02-07 01:38:56 +0000

    good

--- threejs/utils/controlCamera.js
+++ threejs/utils/controlCamera.js
@@ -1,12 +1,18 @@
 import { Vector3 } from "three";
 
-let x  = 1;
+let counter  = 0;
 export function controlCamera(ship, shipProp, camera){
     const offset = new Vector3();
     offset.copy(camera.cameraOffset);
-    offset.z += x;
+    offset.z += counter;
 
     camera.position.copy(ship.position).add(offset);
     const dist = ship.position.distanceTo(camera.position);
-    if(dist < 110 && shipProp.accelarating) x += shipProp.accelaration;
+    if(dist < 110 && shipProp.accelarating) counter += shipProp.accelaration;
+
+    if(!shipProp.accelarating && (shipProp.linearVelocity !== 0 || counter > 0)) {
+        counter -= shipProp.accelaration;
+        
+        counter = counter < 1 ? 0 : counter; 
+    }
 }
