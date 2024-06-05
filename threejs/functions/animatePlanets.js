f1896c071d94d38347b4d42a1ad787fa0c9d8f5c
glutonium69 <146271709+glutonium69@users.noreply.github.com>
2024-06-05 15:14:08 +0000

    AAAAAAAA

--- threejs/functions/animatePlanets.js
+++
@@ -1,6 +0,0 @@
-export function animatePlanets(planets){
-    for(let planet of planets){
-        planet.sphere.rotateY(planet.axisRotation * Math.PI / 180);
-        planet.parent?.rotateY(planet.orbitRotation * Math.PI / 180);
-    }
-}
